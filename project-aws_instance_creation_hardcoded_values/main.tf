4858f9f92c841eeaa103b9340245291156ba5bc9
sreehubpay <102809570+sreehubpay@users.noreply.github.com>
2024-09-15 12:19:58 +0530

    updated provider block with aws credentials

--- project-aws_instance_creation_hardcoded_values/main.tf
+++ project-aws_instance_creation_hardcoded_values/main.tf
@@ -1,12 +1,15 @@
 # create aws instance using the terraform 
 #provide the provider specific details and or authentication details
-provider "aws"{
-    region="us-east-1"
+provider "aws" {
+  region = "us-east-1"
+  #directly configuring the aws credentials in terraform(not recommended) instead of authorising on cli
+  access_key = "your-access-key-id"
+  secret_key = "your-secret-access-key"
 }
 # provide the resource type want to be created along with the details
-resource "aws_instance" "myfirstinstance"{
-    ami="ami-0182f373e66f89c85"
-    instance_type="t2.micro"
+resource "aws_instance" "myfirstinstance" {
+  ami           = "ami-0182f373e66f89c85"
+  instance_type = "t2.micro"
 }
 #print the instance id to the output
 output "myfirstinstance_id" {
