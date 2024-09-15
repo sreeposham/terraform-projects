f35c9fa6a17b5237c5b8063c17b7a910a2bc4052
sreehubpay <102809570+sreehubpay@users.noreply.github.com>
2024-09-15 12:02:42 +0530

    fixed region issue

--- project-aws_instance_creation_hardcoded_values/main.tf
+++ project-aws_instance_creation_hardcoded_values/main.tf
@@ -1,10 +1,14 @@
 # create aws instance using the terraform 
 #provide the provider specific details and or authentication details
 provider "aws"{
-    region="ap-south-1"
+    region="us-east-1"
 }
 # provide the resource type want to be created along with the details
 resource "aws_instance" "myfirstinstance"{
     ami="ami-0182f373e66f89c85"
     instance_type="t2.micro"
+}
+#print the instance id to the output
+output "myfirstinstance_id" {
+  value = aws_instance.myfirstinstance.id
 }
